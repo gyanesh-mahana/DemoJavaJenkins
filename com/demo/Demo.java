b131047aabc1cf3f6ad8f45572d85a2fb7187fe0
gyanesh <gyanesh@gyanesh-ZED-Air-CX3>
2021-01-21 20:21:11 +0530

    demo change

--- com/demo/Demo.java
+++ com/demo/Demo.java
@@ -2,5 +2,7 @@ package com.demo;
 class Demo{
   public static void main(String[] args){
     System.out.println("Hello Jenkins");
+    
+    System.out.println("this is the demo program for Jenkins");
     }
 }
